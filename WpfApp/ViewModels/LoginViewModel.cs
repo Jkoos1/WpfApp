7144473c1424cd26ebc7ae3067f5784faa2b72a2
Justinas <j.ribakau@gmail.com>
2022-07-08 17:38:04 +0300

    Login UI chanes, configured Caliburn.Micro

---
+++ WpfApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace WpfApp.ViewModels {
+
+    public class LoginViewModel : Screen {
+
+        private string _userName;
+        private string _password;
+
+
+    }
+}
